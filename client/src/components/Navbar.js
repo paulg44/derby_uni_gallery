55471ec97b5f952aaa30caa448ff1d0954e1213c
Paul Garton <paulgarton1984@hotmail.com>
2024-12-07 11:49:36 +0000

    add responsivity to navbar: include burger style menu. add responsivity to homepage: align items to center and adjust font sizes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,22 +1,40 @@
 import "../assets/css/navbar.css";
 import { Link } from "react-router-dom";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaBars } from "react-icons/fa";
 import logoImage from "../assets/images/uni_derby_logo.jpg";
+import { useState } from "react";
 
 function Navbar() {
+  const [navMenuOpen, setNavMenuOpen] = useState(false);
+  const [navClass, setNavClass] = useState(<FaBars />);
+
+  const openNavMenu = () => {
+    setNavMenuOpen(!navMenuOpen);
+  };
+
+  const closeNavMenu = () => {
+    setNavMenuOpen(false);
+  };
+
   return (
     <nav className="nav">
       <Link to="/" className="logo">
         <img src={logoImage} alt="logo - University of Derby"></img>
       </Link>
-      <div className="navLinksContainer">
+
+      <div className="navMenu" onClick={openNavMenu}>
+        <FaBars />
+      </div>
+      <div className={`navLinksContainer ${navMenuOpen ? "open" : ""}`}>
         <ul className="navLinks">
           <li>Course Search</li>
           <li>Study</li>
           <li>Life</li>
           <li>International</li>
           <li>Research</li>
-          <Link to="/gallery">Gallery</Link>
+          <Link to="/gallery" onClick={closeNavMenu}>
+            Gallery
+          </Link>
           <li>Alumni</li>
         </ul>
       </div>
