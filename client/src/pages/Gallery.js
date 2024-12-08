70db439c9842ba2391a0d605c6c24e157a27eeb9
Paul Garton <paulgarton1984@hotmail.com>
2024-12-08 17:54:32 +0000

    complete css for gallery and media queries for smartphones

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -1,14 +1,17 @@
+import "../assets/css/gallery.css";
 import data from "../assets/data/journey.json";
 
 function Gallery() {
   return (
-    <div className="galleryContainer">
-      {data.map((step) => (
-        <div className="imageCard" key={step.id}>
-          <img src={step.image} alt={step.alt} />
-          <h3>{step.caption}</h3>
-        </div>
-      ))}
+    <div className="gallery">
+      <div className="galleryCardContainer">
+        {data.map((step) => (
+          <div className="imageCard" key={step.id}>
+            <img src={step.image} alt={step.alt} />
+            <h3>{step.caption}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
